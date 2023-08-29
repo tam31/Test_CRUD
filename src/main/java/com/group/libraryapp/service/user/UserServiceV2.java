eb316bd9315ff4fa3e68487476ce7003f9661be0
tam31 <wxogud3377@naver.com>
2023-08-30 02:18:50 +0900

    트랜잭션과 영속성(변경감지) 이해

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,12 +6,14 @@ import com.group.libraryapp.dto.request.user.UserCreateRequest;
 import com.group.libraryapp.dto.request.user.UserResponse;
 import com.group.libraryapp.dto.request.user.UserUpdateRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 
 @Service
+@Transactional(readOnly = true)
 public class UserServiceV2 {
 
     private final UserRepository userRepository;
@@ -20,6 +22,7 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public void saveUser(UserCreateRequest request){
         userRepository.save(new User(request.getName(), request.getAge()));
     }
@@ -30,13 +33,15 @@ public class UserServiceV2 {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void userUpdate(UserUpdateRequest request){
         User user = userRepository.findById(request.getId())
                 .orElseThrow(IllegalArgumentException::new);
         user.updateName(request.getName());
-        userRepository.save(user);
+//        userRepository.save(user); 변경감지기능으로 영속성컨텐츠로 자동저장
     }
 
+    @Transactional
     public void deleteUser(String name){
         User user = userRepository.findByName(name).orElseThrow(IllegalArgumentException::new);
         userRepository.delete(user);
